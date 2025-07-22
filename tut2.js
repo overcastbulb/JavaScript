bdac40c966c52ebe6982c9acd372a9cdffb4733b
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-07-22 12:43:37 +0530

    Add files via upload

---
+++ tut2.js
@@ -0,0 +1,14 @@
+console.time('Your code took');
+console.log('HELLO CONSOLE');
+console.log(34+8);
+console.log(34);
+console.log(true);
+console.log([34,2,1,20]);
+console.log({Anu:'this',marks:22});
+console.table({Anu:'this',marks:22});
+console.warn('This is a warning');
+// console.clear();
+console.timeEnd('Your code took');
+// console.assert(566<189,'Age >189  is not possible'); 9876
+console.error('This is an error');
+
