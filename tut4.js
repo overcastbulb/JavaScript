777dadc7fe2571b332deb20d84e8c20dd152e2f2
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-07-27 15:03:33 +0530

    Add files via upload

---
+++ tut4.js
@@ -0,0 +1,43 @@
+// Primitive Data types
+
+// String
+let name = "Aniruddha";
+console.log("my string is " + name);
+console.log("Data type is " + (typeof name));
+
+// Numbers
+let marks = 66.6;
+console.log("Data type is " + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log("Data type is " + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log("Data type is " + (typeof nullVar));
+
+// Undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef));
+
+// Reference Data Types
+let myarr = [1, 2, 3, 4, 5, 6, false, "string"];
+console.log("Data type is " + (typeof myarr));
+
+// Object Literals
+let Stmarks ={
+    aniruddha:89,
+    shubham:44,
+    harry:77,
+    rohan:22
+}
+console.log( typeof Stmarks);
+
+function findname() {
+    
+}
+console.log( typeof findname);
+
+let date = new Date();
+console.log( typeof date);
