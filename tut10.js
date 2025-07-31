dc8cc15d78e178ed04061fc665aa3900eba253ea
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-07-31 15:15:30 +0530

    Add files via upload

---
+++ tut10.js
@@ -0,0 +1,43 @@
+console.log("This is tut 10");
+//Watch JS video no.10 at 16 mins.
+
+if(1){
+    var i = 234;
+    console.log(i); //.
+}
+console.log(i); //.
+
+function ui(name)
+{
+    let i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+console.log(ui("Aniruddha"),i); //.
+
+
+// const mygreet = function greet(name, thanks="Thank You"){
+//     let msg = `Here's a tip: } ${name} The correct formatting is “Season's Greetings”—with the apostrophe. When writing the phrase, it is standard English to place the apostrophe after the N and before the S in “seasons.” The apostrophe tells us that the greetings are regarding the current season (and all the holidays in it.${thanks}`
+//     return msg;
+// }
+
+// let name = 'SkillF';
+// let name2 = 'Rohan';
+
+// let val = mygreet (name,'Thanks a lot!');
+// console.log(val);
+
+// const myobj = {
+//     name : "SkillF",
+//     game:function(){
+//         return "GTA";
+//     }
+// }
+// console.log(myobj.game());
+
+// arr = ['fruit','vegetable','furniture'];
+
+// arr.forEach(function(element,index,array)
+// {
+//     console.log(element,index);
+// });
---
+++ tut12.js
@@ -0,0 +1,11 @@
+console.log("Welcome to tut 12.js");
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// });
+a = document.links[0].href;
+console.log(a);
+//use document.images and document.scripts and print the list of images and scipts on an html page
