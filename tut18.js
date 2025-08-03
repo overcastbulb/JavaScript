1de8d83b766f08a1d00baaa27b4d9cf6fb1290f0
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-08-03 17:53:53 +0530

    Add files via upload

---
+++ tut18.js
@@ -0,0 +1,32 @@
+console.log('This is tutorial 18');
+// document.getElementById('btn');
+// btn.addEventListener('click',func1);
+// btn.addEventListener('dblclick',func2);
+// btn.addEventListener('mousedown',func3);
+
+// function func1(e){
+//     console.log("Thanks",e);
+//     e.preventDefault();
+// }
+// function func2(e){
+//     console.log("Thanks its a double click",e);
+//     e.preventDefault();
+// }
+// function func3(e){
+//     console.log("Thanks its a mouse down",e);
+//     e.preventDefault();
+// }
+// document.querySelector('.no').addEventListener('mouseenter',function(){
+//     console.log("You enetered no");
+// })
+
+// document.querySelector('.no').addEventListener('mouseleave',function(){
+//     console.log("You exited no");
+// })
+
+// document.querySelector('.container').addEventListener('mousemove',function(e){
+//     console.log(e.offsetX,e.offsetY);
+//     document.body.style.backgroundColor = 
+//     `rgb(${e.offsetX},${e.offsetX},${e.offsetY})`;
+//     console.log("You triggered mouse move event");
+// })
