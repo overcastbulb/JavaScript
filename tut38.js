66a7cae74ab93995711d837a70eafa8819c02c3c
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-12 16:49:06 +0530

    Add files via upload

---
+++ tut38.js
@@ -0,0 +1,4 @@
+// You have to pretend to use a word api which will contain an object and you have to print all the results from the word api of that word api.
+// You have to take input from the input tag.
+// You have to print it in the DOM.
+// If you are bootstrap then its a plus.
