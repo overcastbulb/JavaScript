d4543e332bffbe725bce189d7812b89a4a5556ab
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-14 13:19:56 +0530

    Add files via upload

---
+++ tut44.js
@@ -0,0 +1,27 @@
+console.log("This is tutorial 44");
+
+// Pretend this is coming from a server as as response.
+
+let a = "Aniruddha Bhai";
+a = undefined;
+if (a != undefined) {
+    throw new Error('This is not undefined');
+}
+else {
+    console.log('This is undefined');
+}
+
+try {
+    // lkjhgfdssa
+    console.log("We are inside try block");
+    functionAnu();   
+    document.getElementById
+
+} catch (error) {
+    console.log("Are you okay?");
+    // console.log(error.name);
+    console.log(error.message);
+    console.log(error);
+} finally {
+    console.log("Finally we will run this");
+}
