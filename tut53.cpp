10f58ee2ecb93b67907aa6a21c58cdba10174179
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-21 15:02:10 +0530

    Add files via upload

---
+++ tut53.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+class A{
+    int a;
+    public:
+    // A & setData (int a)
+        void setData(int a){
+            this->a = a;
+            //return *this;
+        }
+
+        void getData(){
+            cout<<"The value of a is "<<a<<endl;
+        }
+};
+
+int main(){
+    // This is a keyword which is pointer which points to the oblects which invokes the member function
+    A a;
+    a.setData(4);
+    a.getData();
+    return 0;
+}
